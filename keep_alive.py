d4ff7712818c7d6aefe86bba210a46eafdb2a273
nhatminh132 <lpnminh472@gmail.com>
2025-10-22 14:42:04 +0700

    keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,16 @@
+# Tệp: keep_alive.py
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot đang chạy!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
